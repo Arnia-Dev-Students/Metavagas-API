1125d771c49675ee4be503f8bfa274f8ce7cfa78
Marco Aurélio <marcoaurelio_db@hotmail.com>
2024-05-08 20:24:16 -0300

    feat: completed current user and auth guard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -5,11 +5,11 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: ) {}
+  constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
