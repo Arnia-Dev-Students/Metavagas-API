a4b8d5e0bfc65132b470b6921d72abefd9eaf004
paolavalenciavillalobos <paolavalenciavillalobos@gmail.com>
2024-05-09 01:03:07 -0300

    register new user, authmodule/service and controller, hash password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule
+  ],
 })
 export class AppModule {}
