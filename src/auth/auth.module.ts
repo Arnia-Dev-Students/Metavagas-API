6475164d654baca8e5bfb0b56801347ec30c726b
paolavalenciavillalobos <paolavalenciavillalobos@gmail.com>
2024-05-09 22:34:59 -0300

    route login in entity auth, controller, service and edit jwtconfig and auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/database/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtOptions } from './jwt/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.registerAsync({ ...jwtOptions, global: true })],
   controllers: [AuthController],
   providers: [AuthService],
 })
