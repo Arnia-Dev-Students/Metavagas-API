50f14f9f8926a4523c6fb33e7e52bb287fdbd5f3
Marco Aurélio <marcoaurelio_db@hotmail.com>
2024-05-31 16:23:43 -0300

    fix: fix enum text

--- src/enums/successful-message.enum.ts
+++ src/enums/successful-message.enum.ts
@@ -1,6 +1,6 @@
 export enum SUCCESSFUL_MESSAGE {
   DELETE_COMPANY = "Company deleted with success.",
-  DELETE_TECHNOLOGY = "Company deleted with success.",
+  DELETE_TECHNOLOGY = "Technology deleted with success.",
   DELETE_USER = "User deleted with success.",
   DELETE_VACANCY = "Vacancy deleted with success.",
 }
